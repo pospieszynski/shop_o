c60d8098424b14b75eb551cfdf17a5e8123c4c6e
Wojciech Pospieszyski <pospieszynski.wojciech@gmail.com>
2016-07-06 11:55:39 +0200

    moved classes to multiple files

---
+++ shop.rb
@@ -0,0 +1,9 @@
+require_relative './shop_basket.rb'
+require_relative './warehouse.rb'
+require_relative './product.rb'
+
+basket = ShopBasket.new(  WareHouse.new( { Product.new("milk", 5) => 9,  Product.new("bread", 6) => 9 } ) )
+30.times { basket.add "milk"; basket.add "bread" }
+30.times{ basket.remove("milk") }
+basket.receit
+
